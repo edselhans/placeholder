622ab676e50f79abea3f61e61d52ef58ba4b30e2
Eddie <edward.ulick@gmail.com>
2018-09-23 11:39:08 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+(function(){
+  const heading = document.getElementById('heading');
+  for (let i = 1; i <= 7; i++) {
+    let div = document.createElement('div');
+    div.classList.add('heading__title');
+    div.classList.add('heading__title--' + i);
+    div.innerHTML = '<span class="heading__sub">SUB</span><span class="heading__underscore">_</span>A<span class="heading__l">L</span>T';
+    heading.appendChild(div);
+  }
+}());
+
+window.onload = function() {
+  let loadScreen = document.getElementById('load-screen');
+  loadScreen.addEventListener('transitionend', function() {
+    loadScreen.classList.add('remove-load');
+  }, {once: true});
+  window.requestAnimationFrame(function(){
+    loadScreen.style.opacity = 0;
+  });
+};
